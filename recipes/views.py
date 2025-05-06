342e1c746531fcc640346947296f97aaacf8ae88
Vitor Zavan <vitorzavanj@gmail.com>
2025-05-05 22:58:00 -0300

    realocando pastas

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+def contato(request):
+    return HttpResponse('CONTATO')
